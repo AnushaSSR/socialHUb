205f6ebb01da60217ee591be9a1d4afe4c155a95
anusha <nssranusha@yahoo.co.in>
2022-06-24 23:41:59 +0530

    added views to  home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 module.exports.home= function(req,res){
-    res.end('<h1>Express is up for socialHub</h1>');
+    // sending o browesrres.end('<h1>Express is up for socialHub</h1>');
+    //send the response
+    return res.render('home' , {
+        title: "Home"
+    });
 }
 
 module.exports.dashboard = function(req,res) {
--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const port = 8000;
 
 app.use('/', require('./routes'));
 
+//setup view engine, for view setup
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error: ${err}`);
